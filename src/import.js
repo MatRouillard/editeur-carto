b6f550d7f156187cfd09331fa293bce09c712140
MatRouillard <mathis.rouillard@ign.fr>
2025-07-10 18:37:39 +0200

    init: Ajout projet initial sur branch dev

---
+++ src/import.js
@@ -0,0 +1,7 @@
+import 'dsfrign/dist/core/core.module.min.js';
+import 'dsfrign/dist/component/header/header.module.min.js';
+import 'dsfrign/dist/component/navigation/navigation.module.min.js';
+import 'dsfrign/dist/component/button/button.module.min.js';
+import 'dsfrign/dist/component/modal/modal.module.min.js';
+
+import 'geoportal-access-lib/dist/GpServices.js';
